4662747d4a53eede35d5d295e6f93c3eee21d2b5
bkmcco <108086310+bkmcco@users.noreply.github.com>
2025-05-18 23:22:31 -0400

    Add files via upload

---
+++ volt/3000/execute.py
@@ -0,0 +1,20 @@
+import subprocess
+
+
+executable1 = "/afs/cern.ch/user/b/bmcconne/private/garfieldpp/GEM/triple_gem/nd-gar/volt/3000/5atm.py"
+
+
+num_runs = 4
+
+def run_python_script(executable, times):
+    for i in range(times):
+        print(f"Running {executable} - Attempt {i+1}/{times}")
+        try:
+            result = subprocess.run(["python", executable], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # Decode the stdout with 'ignore' to skip undecodable characters
+            print(f"Output:\n{result.stdout.decode(errors='ignore')}")
+        except subprocess.CalledProcessError as e:
+            # Decode the stderr with 'ignore' to skip undecodable characters
+            print(f"Error while running script: {e.stderr.decode(errors='ignore')}")
+
+run_python_script(executable1, num_runs)
